070740a042064c03563d996b42f291ddf8b40934
Shubham Lakhmani <jordanrock@Shubhams-MacBook-Air.local>
2023-04-02 20:27:56 +0530

    Added counting sort function in main function

--- Arrays/Sorting.java
+++ Arrays/Sorting.java
@@ -74,9 +74,10 @@ public class Sorting {
     }
 
     public static void main(String[] args) {
-        int arr[] = { 5, 4, 1, 3, 2 };
+        int arr[] = { 5, 4, 1, 3,1, 2 };
         // bubbleSort(arr);
         // selectionSort(arr);
-        insertionSort(arr);
+        // insertionSort(arr);
+        countingSort(arr);
     }
 }
