aec102c771e83bb1ca5fd0394a1062fbb07c0c61
Shubham Lakhmani <jordanrock@Shubhams-MacBook-Air.local>
2023-04-02 21:57:36 +0530

    Added Spiral code matrix

--- Arrays/Matrices.java
+++ Arrays/Matrices.java
@@ -2,6 +2,46 @@ package Arrays;
 
 public class Matrices {
 
+    public static void printSpiral(int matrix[][]) {
+        int startRow = 0;
+        int endRow = matrix.length - 1;
+        int startCol = 0;
+        int endCol = matrix[0].length - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            //top
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(matrix[startRow][j] + " ");
+            }
+
+            //right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(matrix[i][endCol] + " ");
+            }
+
+            //bottom
+            for (int j = endCol - 1; j >= startCol; j--) {
+                if (startRow == endRow) {
+                    break;
+                }
+                System.out.print(matrix[endRow][j] + " ");
+            }
+
+            //left
+            for (int i = endRow - 1; i > startRow; i--) {
+                if (startCol == endCol) {
+                    break;
+                }
+                System.out.print(matrix[i][startCol] + " ");
+            }
+
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
+        }
+    }
+
     public static void findElement(int matrix[][], int element) {
         boolean found = false;
         for (int i = 0; i < matrix.length; i++) {
@@ -42,8 +82,15 @@ public class Matrices {
 
     public static void main(String[] args) {
         // int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        // int matrix[][] = { { 1, 2, 3, 4}, { 5, 6, 7, 8 } };
+        int matrix[][] = {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
+         };
         // findElement(matrix, 10);
-        findMinAndMax(matrix);
+        // findMinAndMax(matrix);
+        printSpiral(matrix);
     }
 }
