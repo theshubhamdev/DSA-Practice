12cf3184e34a57a18db6565f49c91a6673608e1d
Shubham Lakhmani <jordanrock@Shubhams-MacBook-Air.local>
2023-04-02 22:24:49 +0530

    Done with diagonal sum

--- Arrays/Matrices.java
+++ Arrays/Matrices.java
@@ -9,17 +9,17 @@ public class Matrices {
         int endCol = matrix[0].length - 1;
 
         while (startRow <= endRow && startCol <= endCol) {
-            //top
+            // top
             for (int j = startCol; j <= endCol; j++) {
                 System.out.print(matrix[startRow][j] + " ");
             }
 
-            //right
+            // right
             for (int i = startRow + 1; i <= endRow; i++) {
                 System.out.print(matrix[i][endCol] + " ");
             }
 
-            //bottom
+            // bottom
             for (int j = endCol - 1; j >= startCol; j--) {
                 if (startRow == endRow) {
                     break;
@@ -27,7 +27,7 @@ public class Matrices {
                 System.out.print(matrix[endRow][j] + " ");
             }
 
-            //left
+            // left
             for (int i = endRow - 1; i > startRow; i--) {
                 if (startCol == endCol) {
                     break;
@@ -80,17 +80,60 @@ public class Matrices {
         }
     }
 
+    public static void diagonalSum(int matrix[][]) {
+        if (matrix.length != matrix[0].length) {
+            System.out.println("m should be equal to n");
+            return;
+        }
+        int sum = 0;
+        
+        // Base Approach O(n^2)
+        
+        // for (int i = 0; i < matrix.length; i++) {
+        //     for (int j = 0; j < matrix[i].length; j++) {
+        //         if (i == j) {
+        //             sum += matrix[i][j];
+        //         }
+        //         else if (i + j == matrix.length - 1) {
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+        
+        // Better Approach O(n)
+        for (int i = 0; i < matrix.length; i++) {
+            // primary diagonal
+            sum += matrix[i][i];
+            // secondary diagonal
+            if (i != matrix.length - 1 - i) {
+                sum += matrix[i][matrix.length - 1 - i];
+            }
+        }
+        System.out.println("Diagonal Sum = " + sum);
+    }
+
     public static void main(String[] args) {
-        // int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        // int matrix[][] = { { 1, 2, 3, 4}, { 5, 6, 7, 8 } };
         int matrix[][] = {
-                { 1, 2, 3, 4 },
-                { 5, 6, 7, 8 },
-                { 9, 10, 11, 12 },
-                { 13, 14, 15, 16 }
-         };
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+             };
+        // int matrix[][] = { { 1, 2, 3, 4}, { 5, 6, 7, 8 } };
+        // int matrix[][] = {
+        //         { 1, 2, 3, 4 },
+        //         { 5, 6, 7, 8 },
+        //         { 9, 10, 11, 12 },
+        //         { 13, 14, 15, 16 }
+        // };
+        // int matrix[][] = {
+        //     { 1, 1, 1, 1 },
+        //     { 5, 1, 1, 1 },
+        //     { 9, 1, 1, 12 },
+        //     { 1, 14, 15, 1 }
+        // };
         // findElement(matrix, 10);
         // findMinAndMax(matrix);
-        printSpiral(matrix);
+        // printSpiral(matrix);
+        diagonalSum(matrix);
     }
 }
