fe71516093d441e6cd8a534d1aab2617235fa587
Shubham Lakhmani <jordanrock@Shubhams-MacBook-Air.local>
2023-04-02 20:29:30 +0530

    Fixed trapped rainwater question

--- Arrays/TrappingRainwater.java
+++ Arrays/TrappingRainwater.java
@@ -1,11 +1,36 @@
 package Arrays;
 
 public class TrappingRainwater {
-    public static int trappedRainwater(int height[]) {
-        
+    public static void trappedRainwater(int height[]) {
+        int n = height.length;
+        // Calc Left max boundary - array
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+
+        // Calc Right max boundary - array
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+        // loop
+
+        int trappedwater = 0;
+        for (int i = 0; i < n; i++) {
+            // waterlevel = min(leftmax bound, rightmax bound)
+            int waterlevel = Math.min(leftMax[i], rightMax[i]);
+
+            // trapped water = waterlevel - height[i]
+            trappedwater += waterlevel - height[i];
+        }
+        System.out.println("Trapped Water =" + trappedwater);
     }
     
     public static void main(String[] args) {
-        
+        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        trappedRainwater(height);
     }
 }
